d7039f7f2b977fd0cc128e491f53a09d79403114
Darshan <darshantank3@gmail.com>
2019-08-16 06:41:48 +0530

    Style fixes for ECS of Explode entities

--- src/main/java/org/terasology/combatSystem/weaponFeatures/components/ExplodeComponent.java
+++ src/main/java/org/terasology/combatSystem/weaponFeatures/components/ExplodeComponent.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2019 MovingBlocks
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.terasology.combatSystem.weaponFeatures.components;
 
 import org.terasology.entitySystem.Component;
@@ -10,7 +25,7 @@ import org.terasology.network.Replicate;
  * the other entity may be defined in <b>explosionPrefab</b> or <b>explosionEntity</b>.
  * <b>explosionEntity</b> is given precedence over <b>explosionPrefab</b> if both are present.
  */
-public class ExplodeComponent implements Component{
+public class ExplodeComponent implements Component {
     @Replicate
     public Prefab explosionPrefab;
 }
--- src/main/java/org/terasology/combatSystem/weaponFeatures/systems/collisionHandling/ExplodeHandlingSystem.java
+++ src/main/java/org/terasology/combatSystem/weaponFeatures/systems/collisionHandling/ExplodeHandlingSystem.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2019 MovingBlocks
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.terasology.combatSystem.weaponFeatures.systems.collisionHandling;
 
 import org.terasology.combatSystem.hurting.HurtEvent;
@@ -22,14 +37,14 @@ import org.terasology.world.BlockEntityRegistry;
 import org.terasology.world.block.BlockComponent;
 
 @RegisterSystem
-public class ExplodeHandlingSystem extends BaseComponentSystem{
+public class ExplodeHandlingSystem extends BaseComponentSystem {
     @In
     EntityManager entityManager;
     @In
     BlockEntityRegistry registry;
     
     @ReceiveEvent(components = ExplodeComponent.class)
-    public void explodeOnContact(CollideEvent event, EntityRef entity){
+    public void explodeOnContact(CollideEvent event, EntityRef entity) {
         EntityRef target = event.getOtherEntity();
         
         // damage the other entity
@@ -37,8 +52,8 @@ public class ExplodeHandlingSystem extends BaseComponentSystem{
         
         entity.send(new ExplodeEvent());
         
-        if(target != null && target != EntityRef.NULL && target.exists()){
-            if(target.hasComponent(ExplodeComponent.class)){
+        if (target != null && target != EntityRef.NULL && target.exists()) {
+            if (target.hasComponent(ExplodeComponent.class)) {
                 target.send(new ExplodeEvent());
             }
         }
@@ -47,28 +62,28 @@ public class ExplodeHandlingSystem extends BaseComponentSystem{
     }
     
     @ReceiveEvent(components = ExplodeComponent.class)
-    public void exploding(ExplodeEvent event, EntityRef entity){
+    public void exploding(ExplodeEvent event, EntityRef entity) {
         explode(entity);
     }
     
     @ReceiveEvent(components = ExplodeComponent.class, priority = EventPriority.PRIORITY_HIGH)
-    public void explodeOnDestroy(DestroyEvent event, EntityRef entity){
+    public void explodeOnDestroy(DestroyEvent event, EntityRef entity) {
         explode(entity);
     }
     
     //--------------------------------private methods--------------------------------
     
-    private void explode(EntityRef entity){
+    private void explode(EntityRef entity) {
         LocationComponent location = entity.getComponent(LocationComponent.class);
-        if(location == null){
+        if (location == null) {
             return;
         }
         ExplodeComponent explode = entity.getComponent(ExplodeComponent.class);
         EntityBuilder explosion = entityManager.newBuilder(explode.explosionPrefab);
         
-        if(explosion != null){
+        if (explosion != null) {
             LocationComponent explosionLoc = explosion.getComponent(LocationComponent.class);
-            if(explosionLoc != null){
+            if (explosionLoc != null) {
                 explosionLoc.setWorldPosition(location.getWorldPosition());
                 explosionLoc.setWorldRotation(location.getWorldRotation());
                 explosionLoc.setWorldScale(location.getWorldScale());
@@ -78,21 +93,21 @@ public class ExplodeHandlingSystem extends BaseComponentSystem{
         }
         entity.removeComponent(ExplodeComponent.class);
         
-        if(entity.hasComponent(BlockComponent.class)){
+        if (entity.hasComponent(BlockComponent.class)) {
             BlockComponent block = entity.getComponent(BlockComponent.class);
             Vector3i blockPos = block.getPosition();
             Vector3i offset = new Vector3i();
-            for(int x=-1; x<2; x++){
-                for(int y=-1; y<2; y++){
-                    for(int z=-1; z<2; z++){
-                        if(x == 0 && y == 0 && z == 0){
+            for (int x = -1; x < 2; x++) {
+                for (int y = -1; y < 2; y++) {
+                    for (int z = -1; z < 2; z++) {
+                        if (x == 0 && y == 0 && z == 0) {
                             continue;
                         }
                         offset.set(x, y, z);
                         offset.add(blockPos);
                         
                         EntityRef adjacentBlock = registry.getBlockEntityAt(offset);
-                        if(adjacentBlock.hasComponent(ExplodeComponent.class)){
+                        if (adjacentBlock.hasComponent(ExplodeComponent.class)) {
                             adjacentBlock.send(new ExplodeEvent());
                         }
                     }
@@ -102,10 +117,10 @@ public class ExplodeHandlingSystem extends BaseComponentSystem{
         
         entity.send(new DestroyEvent(EntityRef.NULL, EntityRef.NULL, EngineDamageTypes.EXPLOSIVE.get()));
         
-        if(explosion != null){
+        if (explosion != null) {
             EntityRef explosionEntity = explosion.build();
             
-            if(explosionEntity.hasComponent(ExplosionComponent.class)){
+            if (explosionEntity.hasComponent(ExplosionComponent.class)) {
                 explosionEntity.send(new ExplosionEvent());
             }
         }
