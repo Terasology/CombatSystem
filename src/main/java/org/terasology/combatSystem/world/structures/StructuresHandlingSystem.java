6d6925e5fd51ddd6711a7ba7237ea82058a4e64f
Michael Pollind <polli104@mail.chapman.edu>
2020-11-27 16:02:26 -0800

    feat(JOML): migrate joml structured templates (#60)
    PR: https://github.com/Terasology/StructureTemplates/pull/45

--- src/main/java/org/terasology/combatSystem/world/structures/StructuresHandlingSystem.java
+++ src/main/java/org/terasology/combatSystem/world/structures/StructuresHandlingSystem.java
@@ -2,6 +2,8 @@ package org.terasology.combatSystem.world.structures;
 
 import java.util.Map;
 
+import org.joml.Vector3i;
+import org.joml.Vector3ic;
 import org.terasology.combatSystem.traps.components.ActivateOnPlaceComponent;
 import org.terasology.combatSystem.traps.components.SwitchComponent;
 import org.terasology.combatSystem.world.structures.components.AddSwitchDoorsComponent;
@@ -10,8 +12,6 @@ import org.terasology.entitySystem.entity.EntityRef;
 import org.terasology.entitySystem.event.ReceiveEvent;
 import org.terasology.entitySystem.systems.BaseComponentSystem;
 import org.terasology.entitySystem.systems.RegisterSystem;
-import org.terasology.math.Region3i;
-import org.terasology.math.geom.Vector3i;
 import org.terasology.registry.In;
 import org.terasology.sensors.PhysicalSensorComponent;
 import org.terasology.structureTemplates.components.SpawnBlockRegionsComponent;
@@ -20,12 +20,14 @@ import org.terasology.structureTemplates.events.StructureBlocksSpawnedEvent;
 import org.terasology.structureTemplates.util.BlockRegionTransform;
 import org.terasology.world.BlockEntityRegistry;
 import org.terasology.world.block.Block;
+import org.terasology.world.block.BlockRegion;
+import org.terasology.world.block.BlockRegionIterable;
 import org.terasology.world.block.family.BlockFamily;
 
 import com.google.common.collect.Maps;
 
 @RegisterSystem
-public class StructuresHandlingSystem extends BaseComponentSystem{
+public class StructuresHandlingSystem extends BaseComponentSystem {
     @In
     BlockEntityRegistry registry;
 
@@ -33,39 +35,39 @@ public class StructuresHandlingSystem extends BaseComponentSystem{
     Map<Vector3i, Block> doorBlocks = Maps.newHashMap();
 
     @ReceiveEvent
-    public void activateBlocksOnStructureSpawn(StructureBlocksSpawnedEvent event, EntityRef entity, SpawnBlockRegionsComponent spawnBlockRegions){
+    public void activateBlocksOnStructureSpawn(StructureBlocksSpawnedEvent event, EntityRef entity, SpawnBlockRegionsComponent spawnBlockRegions) {
         BlockRegionTransform transformation = event.getTransformation();
 
         for (RegionToFill regionToFill : spawnBlockRegions.regionsToFill) {
             Block block = regionToFill.blockType;
             BlockFamily family = block.getBlockFamily();
-            if(family.hasCategory("trap")){
-                Region3i region = regionToFill.region;
+            if (family.hasCategory("trap")) {
+                BlockRegion region = regionToFill.region;
                 region = transformation.transformRegion(region);
                 block = transformation.transformBlock(block);
 
-                for(Vector3i blockPos : region){
+                for (Vector3ic blockPos : BlockRegionIterable.region(region).build()) {
                     awakenAndModifyTrap(block, blockPos);
                 }
             }
         }
 
         AddSwitchDoorsComponent switchDoors = entity.getComponent(AddSwitchDoorsComponent.class);
-        if(switchDoors == null){
+        if (switchDoors == null) {
             return;
         }
 
-        for(DoorsToSpawn doors : switchDoors.doorsToSpawn){
+        for (DoorsToSpawn doors : switchDoors.doorsToSpawn) {
             Vector3i switchPos = doors.switchPos;
             switchPos = transformation.transformVector3i(switchPos);
             EntityRef switchEntity = registry.getBlockEntityAt(switchPos);
 
             SwitchComponent switchComp = switchEntity.getComponent(SwitchComponent.class);
-            if(switchComp == null){
+            if (switchComp == null) {
                 continue;
             }
 
-            for(Vector3i doorPos : doors.doorsPos){
+            for (Vector3i doorPos : doors.doorsPos) {
                 doorPos = transformation.transformVector3i(doorPos);
                 EntityRef doorEntity = registry.getBlockEntityAt(doorPos);
 
@@ -77,13 +79,12 @@ public class StructuresHandlingSystem extends BaseComponentSystem{
 
     //-------------------------------private methods-------------------------
 
-    private void awakenAndModifyTrap(Block block, Vector3i blockPos){
+    private void awakenAndModifyTrap(Block block, Vector3ic blockPos) {
         EntityRef trapEntity = registry.getBlockEntityAt(blockPos);
         PhysicalSensorComponent component = trapEntity.getComponent(PhysicalSensorComponent.class);
-        if(component == null){
+        if (component == null) {
             return;
         }
         trapEntity.addComponent(new ActivateOnPlaceComponent());
     }
-
 }
--- src/main/java/org/terasology/combatSystem/world/structures/components/AddSwitchDoorsComponent.java
+++ src/main/java/org/terasology/combatSystem/world/structures/components/AddSwitchDoorsComponent.java
@@ -2,15 +2,15 @@ package org.terasology.combatSystem.world.structures.components;
 
 import java.util.List;
 
+import org.joml.Vector3i;
 import org.terasology.entitySystem.Component;
-import org.terasology.math.geom.Vector3i;
 import org.terasology.network.Replicate;
 import org.terasology.reflection.MappedContainer;
 
 public class AddSwitchDoorsComponent implements Component{
     @Replicate
     public List<DoorsToSpawn> doorsToSpawn;
-    
+
     @MappedContainer
     public static class DoorsToSpawn {
         public Vector3i switchPos;
