ce579da257d20480a4a495c4a32ea49080a699ac
Michael Pollind <mpollind@gmail.com>
2021-01-03 13:54:14 -0800

    feat(JOML): migrate ExplosionHandlingSystem

--- src/main/java/org/terasology/combatSystem/weaponFeatures/systems/collisionHandling/ExplosionHandlingSystem.java
+++ src/main/java/org/terasology/combatSystem/weaponFeatures/systems/collisionHandling/ExplosionHandlingSystem.java
@@ -2,6 +2,8 @@ package org.terasology.combatSystem.weaponFeatures.systems.collisionHandling;
 
 import java.util.List;
 
+import org.joml.AABBf;
+import org.joml.Vector3f;
 import org.terasology.combatSystem.hurting.HurtEvent;
 import org.terasology.combatSystem.hurting.HurtingComponent;
 import org.terasology.combatSystem.weaponFeatures.components.ExplosionComponent;
@@ -18,53 +20,51 @@ import org.terasology.logic.health.DestroyEvent;
 import org.terasology.logic.health.EngineDamageTypes;
 import org.terasology.logic.health.HealthComponent;
 import org.terasology.logic.location.LocationComponent;
-import org.terasology.math.AABB;
-import org.terasology.math.geom.Vector3f;
 import org.terasology.physics.Physics;
 import org.terasology.registry.In;
 
 @RegisterSystem
-public class ExplosionHandlingSystem extends BaseComponentSystem implements UpdateSubscriberSystem{
+public class ExplosionHandlingSystem extends BaseComponentSystem implements UpdateSubscriberSystem {
     @In
     private Time time;
     @In
     private EntityManager entityManager;
     @In
     private Physics physics;
-    
+
     @ReceiveEvent(components = ExplosionComponent.class)
-    public void explosion(ExplosionEvent event, EntityRef entity){
+    public void explosion(ExplosionEvent event, EntityRef entity) {
         ExplosionComponent explosion = entity.getComponent(ExplosionComponent.class);
-        if(explosion == null){
+        if (explosion == null) {
             return;
         }
-        
+
         explosion.explosionStartTime = time.getGameTime();
-        
-        if(explosion.explosionDelayTime > 0.0f){
+
+        if (explosion.explosionDelayTime > 0.0f) {
             return;
         }
-        
+
         explosion.explosionStarted = true;
-        
+
         entity.saveComponent(explosion);
-        
+
         doExplosion(entity);
     }
-    
+
     @ReceiveEvent(components = ExplosionComponent.class, priority = EventPriority.PRIORITY_HIGH)
-    public void explosionOnDestroy(DestroyEvent event, EntityRef entity){
+    public void explosionOnDestroy(DestroyEvent event, EntityRef entity) {
         ExplosionComponent explosion = entity.getComponent(ExplosionComponent.class);
-        if(explosion.explosionStarted){
+        if (explosion.explosionStarted) {
             return;
         }
-        
+
         explosion.explosionStartTime = time.getGameTime();
-        
+
         explosion.explosionStarted = true;
-        
+
         entity.saveComponent(explosion);
-        
+
         doExplosion(entity);
     }
 
@@ -72,65 +72,71 @@ public class ExplosionHandlingSystem extends BaseComponentSystem implements Upda
     public void update(float delta) {
         // TODO Auto-generated method stub
         Iterable<EntityRef> entityList = entityManager.getEntitiesWith(ExplosionComponent.class);
-        for(EntityRef entity : entityList){
+        for (EntityRef entity : entityList) {
             ExplosionComponent explosion = entity.getComponent(ExplosionComponent.class);
-            if(explosion.explosionStartTime < 0.0f){
+            if (explosion.explosionStartTime < 0.0f) {
                 continue;
             }
             float currentGameTime = time.getGameTime();
-            
+
             // starts a delayed explosion;
-            if(!explosion.explosionStarted){
-                if(currentGameTime >= (explosion.explosionStartTime + explosion.explosionDelayTime)){
+            if (!explosion.explosionStarted) {
+                if (currentGameTime >= (explosion.explosionStartTime + explosion.explosionDelayTime)) {
                     explosion.explosionStarted = true;
-                    
+
                     entity.saveComponent(explosion);
-                    
+
                     doExplosion(entity);
                 }
             }
         }
     }
-    
+
     //--------------------private methods-------------------------
-    
+
     //creates a Trigger and collider for exlosion entity.
-    private void doExplosion(EntityRef entity){
+    private void doExplosion(EntityRef entity) {
         ExplosionComponent explosion = entity.getComponent(ExplosionComponent.class);
         LocationComponent location = entity.getComponent(LocationComponent.class);
-        if(explosion == null || location == null){
+        if (explosion == null || location == null) {
             return;
         }
-        
-        if(entity.hasComponent(HealthComponent.class)){
+
+        if (entity.hasComponent(HealthComponent.class)) {
             entity.removeComponent(HealthComponent.class);
         }
-        
-        List<EntityRef> broadPhaseEntities = physics.scanArea(AABB.createCenterExtent(location.getWorldPosition(), 
-                new Vector3f(explosion.radius, explosion.radius, explosion.radius)), explosion.collidesWith);
-        
-        for(EntityRef otherEntity : broadPhaseEntities){
-            if(otherEntity.getId() == entity.getId()){
+
+        Vector3f pos = location.getWorldPosition(new Vector3f());
+        List<EntityRef> broadPhaseEntities = physics.scanArea(new AABBf(
+            pos.x - explosion.radius,
+            pos.y - explosion.radius,
+            pos.z - explosion.radius,
+            pos.x + explosion.radius,
+            pos.y + explosion.radius,
+            pos.z + explosion.radius), explosion.collidesWith);
+
+        for (EntityRef otherEntity : broadPhaseEntities) {
+            if (otherEntity.getId() == entity.getId()) {
                 continue;
             }
             LocationComponent otherEntityLocation = otherEntity.getComponent(LocationComponent.class);
-            if(otherEntityLocation == null){
+            if (otherEntityLocation == null) {
                 continue;
             }
-            
-            float distanceSq = location.getWorldPosition().distanceSquared(otherEntityLocation.getWorldPosition());
-            float radiusSquared = explosion.radius*explosion.radius;
-            float explosionFactor = 1 - (distanceSq/radiusSquared);
-            if(explosionFactor < 0){
+
+            float distanceSq = pos.distanceSquared(otherEntityLocation.getWorldPosition(new Vector3f()));
+            float radiusSquared = explosion.radius * explosion.radius;
+            float explosionFactor = 1 - (distanceSq / radiusSquared);
+            if (explosionFactor < 0) {
                 explosionFactor = 0;
             }
-            
-            if(distanceSq <= radiusSquared){
+
+            if (distanceSq <= radiusSquared) {
                 HurtingComponent hurting = entity.getComponent(HurtingComponent.class);
-                if(hurting == null){
+                if (hurting == null) {
                     return;
                 }
-                hurting.amount = (int) (hurting.amount*explosionFactor);
+                hurting.amount = (int) (hurting.amount * explosionFactor);
                 hurting.damageType = EngineDamageTypes.EXPLOSIVE.get();
                 entity.saveComponent(hurting);
                 entity.send(new HurtEvent(otherEntity));
