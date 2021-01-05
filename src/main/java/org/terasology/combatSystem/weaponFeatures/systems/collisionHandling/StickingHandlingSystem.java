78d59e4fc5b1c0b85bd8d130ef92342a602e98d5
Michael Pollind <polli104@mail.chapman.edu>
2021-01-05 14:31:33 -0800

    feat(JOML): migrate StickingHandlingSystem (#65)

--- src/main/java/org/terasology/combatSystem/weaponFeatures/systems/collisionHandling/StickingHandlingSystem.java
+++ src/main/java/org/terasology/combatSystem/weaponFeatures/systems/collisionHandling/StickingHandlingSystem.java
@@ -3,6 +3,8 @@ package org.terasology.combatSystem.weaponFeatures.systems.collisionHandling;
 import java.util.Iterator;
 import java.util.List;
 
+import org.joml.Quaternionf;
+import org.joml.Vector3f;
 import org.terasology.combatSystem.hurting.HurtEvent;
 import org.terasology.combatSystem.physics.components.GravityComponent;
 import org.terasology.combatSystem.physics.components.MassComponent;
@@ -21,8 +23,6 @@ import org.terasology.logic.health.DestroyEvent;
 import org.terasology.logic.health.EngineDamageTypes;
 import org.terasology.logic.location.Location;
 import org.terasology.logic.location.LocationComponent;
-import org.terasology.math.geom.Quat4f;
-import org.terasology.math.geom.Vector3f;
 import org.terasology.physics.components.TriggerComponent;
 import org.terasology.physics.events.CollideEvent;
 import org.terasology.registry.In;
@@ -31,223 +31,220 @@ import org.terasology.world.block.BlockComponent;
 import com.google.common.collect.Lists;
 
 @RegisterSystem
-public class StickingHandlingSystem extends BaseComponentSystem implements UpdateSubscriberSystem{
+public class StickingHandlingSystem extends BaseComponentSystem implements UpdateSubscriberSystem {
     List<EntityRef> stuckArrows = Lists.newArrayList();
     List<EntityRef> destroyList = Lists.newArrayList();
-    
+
     @In
     Time time;
-    
+
     @ReceiveEvent(components = {StickComponent.class})
-    public void stickingCollision(CollideEvent event, EntityRef entity){
+    public void stickingCollision(CollideEvent event, EntityRef entity) {
         EntityRef target = event.getOtherEntity();
-        if(target.hasComponent(BlockComponent.class)){
+        if (target.hasComponent(BlockComponent.class)) {
             blockSticking(entity, target);
-        }
-        else{
+        } else {
             sticking(entity, target);
         }
-        
+
         // damage the other entity
         entity.send(new HurtEvent(target));
-        
+
         event.consume();
     }
-    
-    @ReceiveEvent( components = StickComponent.class)
-    public void stick(StickEvent event, EntityRef entity){
+
+    @ReceiveEvent(components = StickComponent.class)
+    public void stick(StickEvent event, EntityRef entity) {
         EntityRef target = event.getTarget();
-        if(target.hasComponent(BlockComponent.class)){
+        if (target.hasComponent(BlockComponent.class)) {
             blockSticking(entity, target);
-        }
-        else{
+        } else {
             sticking(entity, target);
         }
-        
+
         // damage the other entity
         entity.send(new HurtEvent(target));
     }
-    
+
     @ReceiveEvent(components = ParentComponent.class, priority = EventPriority.PRIORITY_HIGH)
-    public void removeChildEntities(DestroyEvent event, EntityRef entity){
+    public void removeChildEntities(DestroyEvent event, EntityRef entity) {
         ParentComponent parent = entity.getComponent(ParentComponent.class);
-        if(parent == null){
+        if (parent == null) {
             return;
         }
-        if(parent.children == null){
+        if (parent.children == null) {
             return;
         }
-        
+
         Iterator<EntityRef> childrenList = parent.children.iterator();
-        while(childrenList.hasNext()){
+        while (childrenList.hasNext()) {
             EntityRef children = childrenList.next();
             children.send(new DestroyEvent(entity, event.getInstigator(), EngineDamageTypes.DIRECT.get()));
         }
     }
-    
+
     @Override
     public void update(float delta) {
-        for(EntityRef arrow : stuckArrows){
-            if(arrow == null || arrow == EntityRef.NULL || !arrow.exists()){
+        for (EntityRef arrow : stuckArrows) {
+            if (arrow == null || arrow == EntityRef.NULL || !arrow.exists()) {
                 destroyList.add(arrow);
                 continue;
             }
             StickComponent stick = arrow.getComponent(StickComponent.class);
-            if(stick == null){
+            if (stick == null) {
                 destroyList.add(arrow);
                 continue;
             }
             float currentGameTime = time.getGameTime();
-            if(currentGameTime >= stick.stickTime + stick.totalStickingTime){
+            if (currentGameTime >= stick.stickTime + stick.totalStickingTime) {
                 destroyList.add(arrow);
             }
         }
-        
-        for(EntityRef arrow : destroyList){
+
+        for (EntityRef arrow : destroyList) {
             stuckArrows.remove(arrow);
             arrow.send(new DestroyEvent(arrow, arrow, EngineDamageTypes.DIRECT.get()));
         }
-        
+
         destroyList.clear();
     }
-    
+
     //------------------------------private methods-----------------------
-    
-    private void sticking(EntityRef entity, EntityRef target){
-        if(target == EntityRef.NULL || target == null){
+
+    private void sticking(EntityRef entity, EntityRef target) {
+        if (target == EntityRef.NULL || target == null) {
             return;
         }
         LocationComponent location = entity.getComponent(LocationComponent.class);
         LocationComponent otherEntityLocation = target.getComponent(LocationComponent.class);
-        if(location == null || otherEntityLocation == null){
+        if (location == null || otherEntityLocation == null) {
             return;
         }
-        
+
         // offset of the point of collision on the colliding entity to the
         // location of the colliding entity
-        Vector3f offset = new Vector3f(location.getWorldPosition());
-        offset.sub(otherEntityLocation.getWorldPosition());
-        
+        Vector3f offset = location.getWorldPosition(new Vector3f());
+        offset.sub(otherEntityLocation.getWorldPosition(new Vector3f()));
+
         // to find quaternion rotation that if applied to quaternion 1 (the one which is
         // inversed) would result in quaternion 2 (the one which is multiplied)
-        Quat4f relativeRot = new Quat4f(otherEntityLocation.getWorldRotation());
-        relativeRot.inverse();
-        relativeRot.mul(location.getWorldRotation());
-        
+        Quaternionf relativeRot = otherEntityLocation.getWorldRotation(new Quaternionf());
+        relativeRot.invert();
+        relativeRot.mul(location.getWorldRotation(new Quaternionf()));
+
         // world scale of the entity.
         float scale = location.getWorldScale();
-        
+
         // resting all the movements of the entity
         MassComponent body = entity.getComponent(MassComponent.class);
-        if(body != null){
+        if (body != null) {
             pierce(entity);
-            
+
             body.acceleration.set(0, 0, 0);
             body.velocity.set(0, 0, 0);
             body.force.set(0, 0, 0);
             entity.saveComponent(body);
         }
-        
-        if(entity.hasComponent(TriggerComponent.class)){
+
+        if (entity.hasComponent(TriggerComponent.class)) {
             entity.removeComponent(TriggerComponent.class);
         }
-        
+
         // attaching entity to otherEntity so that it always follows otherEntity
         Location.attachChild(target, entity, offset, relativeRot, scale);
-        
+
         StickComponent stick = entity.getComponent(StickComponent.class);
         stick.setTarget(target);
         stick.stickTime = time.getGameTime();
         entity.saveComponent(stick);
-        
+
         ParentComponent parent = target.getComponent(ParentComponent.class);
-        if(parent == null){
+        if (parent == null) {
             parent = new ParentComponent();
         }
-        
+
         parent.children.add(entity);
-        
+
         target.addOrSaveComponent(parent);
-        
-        if(entity.hasComponent(GravityComponent.class)){
+
+        if (entity.hasComponent(GravityComponent.class)) {
             entity.removeComponent(GravityComponent.class);
         }
     }
-    
+
     // if the entity wants to stick to a block
-    private void blockSticking(EntityRef entity, EntityRef target){
-        if(target == EntityRef.NULL || target == null){
+    private void blockSticking(EntityRef entity, EntityRef target) {
+        if (target == EntityRef.NULL || target == null) {
             return;
         }
-        
+
         BlockComponent block = target.getComponent(BlockComponent.class);
-        if(block == null){
+        if (block == null) {
             return;
         }
-        if(block.getBlock().isPenetrable()){
+        if (block.block.isPenetrable()) {
             return;
         }
         LocationComponent location = entity.getComponent(LocationComponent.class);
-        if(location == null){
+        if (location == null) {
             return;
         }
-        
+
         MassComponent body = entity.getComponent(MassComponent.class);
-        if(body != null){
+        if (body != null) {
             pierce(entity);
-            
+
             // resting all the movements of the entity
             body.acceleration.set(0, 0, 0);
             body.velocity.set(0, 0, 0);
             body.force.set(0, 0, 0);
             entity.saveComponent(body);
         }
-        
-        if(!target.hasComponent(BlockDamagedComponent.class)){
+
+        if (!target.hasComponent(BlockDamagedComponent.class)) {
             target.addComponent(new BlockDamagedComponent());
         }
-        
+
         ParentComponent parent = target.getComponent(ParentComponent.class);
-        if(parent == null){
+        if (parent == null) {
             parent = new ParentComponent();
         }
         parent.children.add(entity);
         target.addOrSaveComponent(parent);
-        
+
         StickComponent stick = entity.getComponent(StickComponent.class);
         stick.setTarget(target);
         stick.stickTime = time.getGameTime();
         entity.saveComponent(stick);
-        
-        if(entity.hasComponent(GravityComponent.class)){
+
+        if (entity.hasComponent(GravityComponent.class)) {
             entity.removeComponent(GravityComponent.class);
         }
-        
-        if(entity.hasComponent(TriggerComponent.class)){
+
+        if (entity.hasComponent(TriggerComponent.class)) {
             entity.removeComponent(TriggerComponent.class);
         }
     }
-    
-    public void pierce(EntityRef entity){
+
+    public void pierce(EntityRef entity) {
         LocationComponent location = entity.getComponent(LocationComponent.class);
-        if(location == null){
+        if (location == null) {
             return;
         }
-        
+
         StickComponent stick = entity.getComponent(StickComponent.class);
-        Vector3f entityLoc = location.getWorldPosition();
-        
-        Vector3f direction = location.getWorldDirection();
-        direction.scale(stick.pierceAmount);
+        Vector3f entityLoc = location.getWorldPosition(new Vector3f());
+        Vector3f direction = location.getWorldDirection(new Vector3f());
+
+        direction.mul(stick.pierceAmount);
         entityLoc.add(direction);
-        
+
         location.setWorldPosition(entityLoc);
-        
+
         entity.saveComponent(location);
-        
-        if(stick.totalStickingTime >= 0){
+
+        if (stick.totalStickingTime >= 0) {
             stuckArrows.add(entity);
         }
     }
-
 }
