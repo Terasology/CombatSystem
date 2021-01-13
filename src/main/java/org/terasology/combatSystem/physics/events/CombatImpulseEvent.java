49be4b9e05bf9402f229be0a7c48196aff51ab4b
Michael Pollind <polli104@mail.chapman.edu>
2021-01-13 14:13:12 -0800

    feat(JOML): migrate CombatPhysicsSystem (#68)

--- src/main/java/org/terasology/combatSystem/physics/events/CombatImpulseEvent.java
+++ src/main/java/org/terasology/combatSystem/physics/events/CombatImpulseEvent.java
@@ -1,23 +1,23 @@
 package org.terasology.combatSystem.physics.events;
 
+import org.joml.Vector3f;
+import org.joml.Vector3fc;
 import org.terasology.combatSystem.physics.components.MassComponent;
 import org.terasology.entitySystem.event.Event;
-import org.terasology.math.geom.Vector3f;
 
 /**
  * Add <>bImpulse</b> to entities with {@code MassComponent}
  * <b>
  * this changes {@code MassComponent#velocity} variable in {@link MassComponent}
  */
-public class CombatImpulseEvent implements Event{
-    Vector3f impulse;
-    
-    public CombatImpulseEvent(Vector3f impulse){
-        this.impulse = impulse;
-    }
-    
-    public Vector3f getImpulse(){
-        return impulse;
+public class CombatImpulseEvent implements Event {
+    private Vector3f impulse = new Vector3f();
+
+    public CombatImpulseEvent(Vector3fc impulse) {
+        this.impulse.set(impulse);
     }
 
+    public Vector3fc getImpulse() {
+        return impulse;
+    }
 }
