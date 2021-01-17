3d732f4bb45499eb6d55fd4f7f2a334e85373e17
Michael Pollind <mpollind@gmail.com>
2021-01-17 10:49:54 -0800

    feat(JOML): migrate MeleeAttackSystem

--- src/main/java/org/terasology/combatSystem/weaponFeatures/events/PrimaryAttackEvent.java
+++ src/main/java/org/terasology/combatSystem/weaponFeatures/events/PrimaryAttackEvent.java
@@ -1,16 +1,16 @@
 package org.terasology.combatSystem.weaponFeatures.events;
 
+import org.joml.Vector3f;
+import org.joml.Vector3fc;
 import org.terasology.entitySystem.entity.EntityRef;
 import org.terasology.entitySystem.event.Event;
 import org.terasology.logic.common.ActivateEvent;
 import org.terasology.logic.location.LocationComponent;
-import org.terasology.math.JomlUtil;
-import org.terasology.math.geom.Vector3f;
 
 /**
  * Triggered in case of a primary attack
  */
-public class PrimaryAttackEvent implements Event{
+public class PrimaryAttackEvent implements Event {
     private EntityRef instigator;
     private EntityRef target;
     private Vector3f origin;
@@ -22,28 +22,30 @@ public class PrimaryAttackEvent implements Event{
     /**
      * Default constructor required for persistence
      */
-    public PrimaryAttackEvent(){
+    public PrimaryAttackEvent() {
 
     }
 
 
     /**
      * Creates an event based on an ActivateEvent.
+     *
      * @param info The ActivateEvent to base this event off of.
      */
-    public PrimaryAttackEvent(ActivateEvent info){
+    public PrimaryAttackEvent(ActivateEvent info) {
         instigator = info.getInstigator();
         target = info.getTarget();
-        origin = JomlUtil.from(info.getOrigin());
-        direction = JomlUtil.from(info.getDirection());
-        hitPosition = JomlUtil.from(info.getHitPosition());
-        hitNormal = JomlUtil.from(info.getHitNormal());
+        origin = info.getOrigin();
+        direction = info.getDirection();
+        hitPosition = info.getHitPosition();
+        hitNormal = info.getHitNormal();
         activationId = info.getActivationId();
     }
 
 
     /**
      * Gets the instigator of this event.
+     *
      * @return the instigator of this event.
      */
     public EntityRef getInstigator() {
@@ -52,6 +54,7 @@ public class PrimaryAttackEvent implements Event{
 
     /**
      * Gets the target of the attack.
+     *
      * @return the target of the attack.
      */
     public EntityRef getTarget() {
@@ -60,38 +63,43 @@ public class PrimaryAttackEvent implements Event{
 
     /**
      * Gets the origin position of the attack.
+     *
      * @return the origin position of the attack.
      */
-    public Vector3f getOrigin() {
+    public Vector3fc getOrigin() {
         return origin;
     }
 
     /**
      * Gets the direction position of the attack
+     *
      * @return the direction position of the attack.
      */
-    public Vector3f getDirection() {
+    public Vector3fc getDirection() {
         return direction;
     }
 
     /**
      * Gets position where target is hit
+     *
      * @return position where target is hit
      */
-    public Vector3f getHitPosition() {
+    public Vector3fc getHitPosition() {
         return hitPosition;
     }
 
     /**
      * Gets position of normal force perpendicular to hit position
+     *
      * @return position of normal force perpendicular to hit position
      */
-    public Vector3f getHitNormal() {
+    public Vector3fc getHitNormal() {
         return hitNormal;
     }
 
     /**
      * Gets activation ID
+     *
      * @return activation ID
      */
     public int getActivationId() {
@@ -100,26 +108,27 @@ public class PrimaryAttackEvent implements Event{
 
     /**
      * Check if target location is null and gets target position
+     *
      * @return target location
      */
     public Vector3f getTargetLocation() {
         LocationComponent loc = target.getComponent(LocationComponent.class);
         if (loc != null) {
-            return loc.getWorldPosition();
+            return loc.getWorldPosition(new Vector3f());
         }
         return null;
     }
 
     /**
      * Check if instigator location is null and gets instigator position
+     *
      * @return instigator position
      */
     public Vector3f getInstigatorLocation() {
         LocationComponent loc = instigator.getComponent(LocationComponent.class);
         if (loc != null) {
-            return loc.getWorldPosition();
+            return loc.getWorldPosition(new Vector3f());
         }
         return new Vector3f();
     }
-
 }
